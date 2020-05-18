eba6287055602ec89e1c69f224665af340eda7f1
miranda <miranda@LAPTOP-R0JVELH6.localdomain>
2020-05-18 13:28:04 +0200

    kata monday

--- kata.js
+++ kata.js
@@ -1,3 +1,13 @@
 function solution(str) {
   return str.split('').reverse().join('');
 }
+
+function countSheeps(arrayOfSheep) {
+  var amountOfSheep = 0;
+  for (let i = 0; i < arrayOfSheep.length; i++) {
+    if (arrayOfSheep[i] === true) {
+      amountOfSheep++;
+    }
+  }
+  return amountOfSheep
+}
